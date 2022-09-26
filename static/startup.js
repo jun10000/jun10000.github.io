b82d74debd1ee9375b71a203efdd435032d0c000
jun10000 <jun10000@icloud.com>
2022-09-26 17:10:49 +0900

    Fix styles of the new page what shows the imgur image.

--- static/startup.js
+++ static/startup.js
@@ -10,7 +10,7 @@ function open_image_tab(img) {
     page.document.open();
     page.document.write('<html lang="ja"><head>');
     page.document.write('<title>' + img + '</title>');
-    page.document.write('</head><body>');
+    page.document.write('</head><body style="margin: 0; padding: 0;">');
     page.document.write('<img src="' + img + '" alt="' + img + '">');
     page.document.write('</body></html>');
     page.document.close();
